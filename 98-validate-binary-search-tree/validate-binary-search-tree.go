548bcd1c181aa92f04cebac96124015b9fa9bf57
rishavkrm <88821153+rishavkrm@users.noreply.github.com>
2025-05-16 10:31:25 +0530

    Time: 1 ms (14.90%) | Memory: 8.5 MB (6.08%) - LeetSync

---
+++ 98-validate-binary-search-tree/validate-binary-search-tree.go
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isValidBST(root *TreeNode) bool {
+    arr := make([]int, 0)
+    stack := make([]*TreeNode, 0)
+    curr := root
+
+    for curr != nil || len(stack) > 0 {
+        for curr != nil{
+            stack = append(stack, curr)
+            curr = curr.Left
+        }
+        popped := stack[len(stack)-1]
+        stack = stack[:len(stack)-1]
+        arr = append(arr, popped.Val)
+        curr = popped.Right
+    }
+    fmt.Println(arr)
+    return isStrictlyIncreasing(arr)
+}
+
+func isStrictlyIncreasing(arr []int) bool{
+    for i := 0; i < len(arr)-1; i++{
+        if arr[i+1] <= arr[i]{
+            return false
+        }
+    }
+    return true
+}
