b24aa192b2d169b758cd54a0742818a4417461cb
rishavkrm <88821153+rishavkrm@users.noreply.github.com>
2025-05-30 12:43:18 +0530

    Time: 15 ms (88.63%) | Memory: 10.3 MB (49.26%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.go
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.go
@@ -1,26 +1,30 @@
+// 1. make a min-heap with first k elements which will store k largest elements
+// 2. root of the heap is the smallest of the heap
+// 3. if a number is greater than root that means it comes in heap and 
+// replace it with the number and heapify down
+// 4. if a number is smaller then not eligible
+// 5. root will be kth largeest number
+
 func findKthLargest(nums []int, k int) int {
-    // first make a heap of size k
-    n := len(nums)
-    heap := nums[:k]
-    heap = MakeHeap(heap)
-    if n <= k{
-        return heap[0]
+    heapLength := k
+    heap := nums[:heapLength]
+    for i := k/2 - 1; i >= 0; i--{
+        minHeapify(heap, k, i)
     }
-    for i := k; i < n; i++{
-        if heap[0] >= nums[i]{
-            continue
+    for i := k; i < len(nums); i++{
+        if nums[i] > heap[0]{
+            heap[0] = nums[i]
+            minHeapify(heap, k, 0)
         }
-        heap[0] = nums[i]
-        heap = Heapify(heap, k, 0)
     }
     return heap[0]
 }
 
-func Heapify(nums []int, n, i int)[]int{
+// make an array a min-heap
+func minHeapify(nums []int, n int, i int){
     smallest := i
     left := 2 * i + 1
     right := 2 * i + 2
-
     if left < n && nums[left] < nums[smallest]{
         smallest = left
     }
@@ -29,16 +33,6 @@ func Heapify(nums []int, n, i int)[]int{
     }
     if i != smallest{
         nums[i], nums[smallest] = nums[smallest], nums[i]
-        Heapify(nums, n, smallest)
+        minHeapify(nums, n, smallest)
     }
-    return nums
-}
-
-func MakeHeap(arr []int) []int{
-    // first make it heap 
-    n := len(arr)
-    for i := n/2 ; i >= 0; i--{
-        arr = Heapify(arr, n, i)
-    }
-    return arr
 }
