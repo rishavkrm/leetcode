1dfa0cefccd9ce41d1747f8039b06abda0925455
rishavkrm <88821153+rishavkrm@users.noreply.github.com>
2025-06-09 17:29:53 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (91.72%) - LeetSync

--- 135-candy/candy.go
+++ 135-candy/candy.go
@@ -1,26 +1,19 @@
 func candy(ratings []int) int {
     n := len(ratings)
-    candies := make([]int, n)
+    candies := make([]int, len(ratings))
     for i := 0; i < n; i++{
         candies[i] = 1
     }
-    // left to right
     for i := 1; i < n; i++{
         if ratings[i] > ratings[i-1]{
             candies[i] = candies[i-1] + 1
         }
     }
-    // right to left
-    for i := n-2; i >= 0; i--{
+    for i := n-2; i >= 0 ; i--{
         if ratings[i] > ratings[i+1]{
-            // candies[i+1] according to right to left but candies[i] according to the last one
-            // so max of both
-            candies[i] = max(candies[i], candies[i+1] + 1)
+            candies[i] = max(candies[i+1] + 1, candies[i])
         }
     }
-    // 
-    fmt.Println(candies)
-
     sum := 0
     for i := 0; i < n; i++{
         sum += candies[i]
