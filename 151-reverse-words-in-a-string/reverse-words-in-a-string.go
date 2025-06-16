9db9574e2357fb97d1b8fedc7b3c8a2a5790fc3a
rishavkrm <88821153+rishavkrm@users.noreply.github.com>
2025-06-16 21:21:00 +0530

    Time: 6 ms (19.20%) | Memory: 10.1 MB (5.90%) - LeetSync

---
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.go
@@ -0,0 +1,27 @@
+func reverseWords(s string) string {
+	res := ""
+	n := len(s) - 1
+	i := n
+	for i > -1 {
+		for i > -1 && s[i] == 32 {
+			i -= 1
+		}
+		m := i
+		for i > -1 && s[i] != 32 {
+			i -= 1
+		}
+		if m > i {
+			res += s[i+1 : m+1]
+		}
+		res += " "
+	}
+	k := len(res)-1
+	for k >= 0 {
+		if res[k] == 32 {
+			k -= 1
+		} else {
+			break
+		}
+	}
+	return res[0:k+1]
+}
