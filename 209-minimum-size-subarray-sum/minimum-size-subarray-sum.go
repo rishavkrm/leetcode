290452d01f46c3406cb162c7c76a1a9dcf18b96b
rishavkrm <88821153+rishavkrm@users.noreply.github.com>
2025-06-19 22:24:03 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (52.75%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.go
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.go
@@ -1,23 +1,23 @@
+// Start with a minLength of len(array) + 1, and two pointers start and end
+// maintain a currSum
+// if currSum >= target, then update the minLength and decrease the start
+// otherwise (currSum < target), then increase end and continue the process
+
 func minSubArrayLen(target int, nums []int) int {
 	minLength := len(nums) + 1
 	start := 0
 	end := 0
 	currSum := nums[0]
 	for end < len(nums) {
-		if currSum == target {
+		if currSum >= target {
 			minLength = min(minLength, end-start+1)
 			currSum -= nums[start]
 			start += 1
-		} else if currSum < target {
+		} else {
 			if end+1 < len(nums) {
 				currSum += nums[end+1]
 			}
 			end += 1
-		} else {
-			minLength = min(minLength, end-start+1)
-			currSum -= nums[start]
-			start += 1
-
 		}
 
 	}
@@ -27,6 +27,3 @@ func minSubArrayLen(target int, nums []int) int {
 	}
 	return minLength
 }
-
-// 2, 5, 6, 8, 12, 15
-// 
