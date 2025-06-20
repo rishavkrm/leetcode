08910430f862c1ec3643af841e2537a152b21771
rishavkrm <88821153+rishavkrm@users.noreply.github.com>
2025-06-20 22:31:45 +0530

    Time: 19 ms (72.91%) | Memory: 5.3 MB (52.27%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.go
@@ -0,0 +1,48 @@
+func minWindow(s string, t string) string {
+	res := ""
+	start := 0
+	end := 0
+	minLength := len(s) + 100
+
+	T := make(map[byte]int)
+	S := make(map[byte]int)
+
+	count := 0
+
+	for i := range len(t) {
+		T[t[i]] += 1
+	}
+
+	for end < len(s) {
+		curr := s[end]
+		
+		S[curr] += 1
+		if T[curr] > 0 && S[curr] == T[curr] {
+            count += 1
+			if count >= len(T) { 
+				for start <= end {
+					minLength = min(end-start+1, minLength)
+					if minLength == end-start+1 {
+						res = s[start : end+1]
+					}
+					S[s[start]] -= 1 
+					if S[s[start]] < T[s[start]] {
+						start += 1
+						count -= 1
+						break
+					}
+					start += 1
+
+				}
+				end += 1
+
+			} else {
+				end += 1
+			}
+
+		} else {
+			end += 1
+		}
+	}
+	return res
+}
