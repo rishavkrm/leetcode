1b816189cb76fbefdd25cd65464e86a4e303d8ee
rishavkrm <88821153+rishavkrm@users.noreply.github.com>
2025-07-08 17:25:45 +0530

    Time: 2 ms (97.66%) | Memory: 10.6 MB (26.00%) - LeetSync

--- 49-group-anagrams/group-anagrams.go
+++ 49-group-anagrams/group-anagrams.go
@@ -3,18 +3,18 @@ func groupAnagrams(strs []string) [][]string {
 	res := [][]string{}
 	for i := range len(strs) {
 		curr := strs[i]
-		dict[sortedString(curr)] = append(dict[sortedString(curr)], curr)
+		dict[makeIdOfString(curr)] = append(dict[makeIdOfString(curr)], curr)
 	}
-    for _, val := range(dict){
-        res = append(res, val)
-    }
-    return res
+	for _, val := range dict {
+		res = append(res, val)
+	}
+	return res
 }
 
-func sortedString(input string) string {
-	bytes := []byte(input)
-	sort.Slice(bytes, func(a, b int) bool {
-		return bytes[a] < bytes[b]
-	})
-	return string(bytes)
+func makeIdOfString(input string) string {
+	dict := make([]byte, 26)
+	for i := 0; i < len(input); i++ {
+		dict[input[i]-97] += 1
+	}
+	return string(dict)
 }
