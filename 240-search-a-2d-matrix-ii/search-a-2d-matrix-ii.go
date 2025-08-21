05db34a072ad391a46f382396321f6ee517fe13a
rishavkrm <88821153+rishavkrm@users.noreply.github.com>
2025-08-21 16:25:28 +0530

    Time: 19 ms (25.82%) | Memory: 8.5 MB (26.76%) - LeetSync

---
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.go
@@ -0,0 +1,14 @@
+func searchMatrix(matrix [][]int, target int) bool {
+	i := 0
+	j := len(matrix[0]) - 1
+	for i < len(matrix) && j >= 0 {
+		if matrix[i][j] == target {
+			return true
+		} else if matrix[i][j] > target {
+			j -= 1
+		} else {
+			i += 1
+		}
+	}
+    return false
+}
