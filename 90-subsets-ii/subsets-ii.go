ddfc8ac38c0c0089c573b509c23a22bd60870a20
rishavkrm <88821153+rishavkrm@users.noreply.github.com>
2025-08-23 14:08:07 +0530

    Time: 4 ms (2.58%) | Memory: 4.9 MB (12.90%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.go
@@ -0,0 +1,30 @@
+func subsetsWithDup(nums []int) [][]int {
+	sort.Slice(nums, func(i int, j int) bool {
+		return nums[i] > nums[j]
+	})
+	result := make([][]int, 0)
+	curr := make([]int, 0)
+	helper(nums, 0, curr, &result)
+	return result
+}
+
+func helper(nums []int, index int, curr []int, result *[][]int) {
+	fmt.Println(curr)
+	*result = append(*result, copyArray(curr))
+	for i := index; i < len(nums); i++ {
+		if i > index && nums[i-1] == nums[i] {
+			continue
+		}
+		curr = append(curr, nums[i])
+		helper(nums, i+1, curr, result)
+		curr = curr[:len(curr)-1]
+	}
+}
+
+func copyArray(nums []int) []int {
+	res := make([]int, 0)
+	for i := 0; i < len(nums); i++ {
+		res = append(res, nums[i])
+	}
+	return res
+}
