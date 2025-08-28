b2180af5ba23465e7ced3e1555ab5ea2cd3b4ad0
rishavkrm <88821153+rishavkrm@users.noreply.github.com>
2025-08-28 15:59:50 +0530

    Time: 0 ms (100.00%) | Memory: 4.1 MB (11.23%) - LeetSync

--- 75-sort-colors/sort-colors.go
+++ 75-sort-colors/sort-colors.go
@@ -1,29 +1,19 @@
-func sortColors(nums []int) {
-	red := 0
-	blue := len(nums) - 1
-	for i, num := range nums {
-		if num == 0 {
-			if nums[red] == 0 {
-				red += 1
-				continue
-			}
-			temp := nums[red]
-			nums[red] = 0
-			nums[i] = temp
-			red += 1
-		}
-	}
-	for i, _ := range nums {
-		num := nums[len(nums)-1-i]
-		if num == 2 {
-			if nums[blue] == 2 {
-				blue -= 1
-				continue
-			}
-			temp := nums[blue]
-			nums[blue] = 2
-			nums[len(nums)-i-1] = temp
-			blue -= 1
-		}
-	}
+func sortColors(nums []int)  {
+    low, mid, high := 0, 0, len(nums)-1
+    for mid <= high{
+        curr := nums[mid]
+        if curr == 0{
+            nums[mid], nums[low] = nums[low], nums[mid]
+            mid += 1
+            low += 1
+        }
+        if curr == 1{
+            mid += 1
+        }
+        if curr == 2{
+            nums[mid], nums[high] = nums[high], nums[mid]
+            high -= 1
+        }
+    }
+
 }
