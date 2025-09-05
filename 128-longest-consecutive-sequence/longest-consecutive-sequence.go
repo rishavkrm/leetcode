654b194b1957e26f8aeeb23d7cc77bf9f8e22e9f
rishavkrm <88821153+rishavkrm@users.noreply.github.com>
2025-09-05 21:40:30 +0530

    Time: 159 ms (5.10%) | Memory: 19.9 MB (5.10%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.go
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.go
@@ -1,22 +1,60 @@
 func longestConsecutive(nums []int) int {
-	hMap := make(map[int]int)
+	parent := make([]int, len(nums))
+    rank := make([]int, len(nums))
+	hash := map[int]int{}
+	for i := range len(parent) {
+		parent[i] = i
+		hash[nums[i]] = i
+	}
 	for i := 0; i < len(nums); i++ {
-		hMap[nums[i]] = 1
-	}
-	maxi := 0
-	i := 0
-for curr, _ := range hMap {
-		if hMap[curr-1] == 0 {
-			count := 1
-			for hMap[curr+1] == 1 {
-				count += 1
-				curr += 1
-			}
-			maxi = max(maxi, count)
+		curr := nums[i]
+		_, exists := hash[curr+1]
+		if exists {
+			union(hash[curr], hash[curr+1], &parent, &rank)
 		}
-		i += 1
-
+        _, exists = hash[curr-1]
+		if exists {
+			union(hash[curr], hash[curr-1], &parent, &rank)
+		}
 	}
-	return maxi
+    for i := 0; i < len(nums); i++ {
+		curr := nums[i]
+		_, exists := hash[curr+1]
+		if exists {
+			union(hash[curr], hash[curr+1], &parent, &rank)
+		}
+        _, exists = hash[curr-1]
+		if exists {
+			union(hash[curr], hash[curr-1], &parent, &rank)
+		}
+	}
+    maxi := 0
+    nHash := map[int]int{}
+    for _,i := range parent{
+        nHash[i] += 1
+        maxi = max(maxi, nHash[i])
+    }
+    return maxi
+}
 
+func find(k int, parent *[]int) int {
+	if !((*parent)[k] == k) {
+		(*parent)[k] = find((*parent)[k], parent)
+	}
+	return (*parent)[k]
+}
+func union(x1 int, x2 int, p *[]int, r *[]int) {
+	x1Parent := find(x1, p)
+	x2Parent := find(x2, p)
+	if x1Parent == x2Parent {
+		return
+	}
+	if (*r)[x1Parent] > (*r)[x2Parent] {
+		(*p)[x2Parent] = x1Parent
+	} else if (*r)[x1Parent] < (*r)[x2Parent] {
+		(*p)[x1Parent] = x2Parent
+	} else {
+		(*p)[x1Parent] = x2Parent
+		(*r)[x2Parent] += 1
+	}
 }
