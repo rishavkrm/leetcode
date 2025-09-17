f0cedbf6b77eb225d5305d970122e2774bed7a05
rishavkrm <88821153+rishavkrm@users.noreply.github.com>
2025-09-17 15:06:28 +0530

    Time: 68 ms (5.13%) | Memory: 15 MB (15.38%) - LeetSync

--- 491-non-decreasing-subsequences/non-decreasing-subsequences.go
+++ 491-non-decreasing-subsequences/non-decreasing-subsequences.go
@@ -8,7 +8,7 @@ func findSubsequences(nums []int) [][]int {
 func helper(nums []int, i int, currMax int, temp []int, result *[][]int, hash map[string]bool) {
 	if i >= len(nums) {
 		if len(temp) >= 2 {
-            curr := createString(temp)
+			curr := createString(temp)
 			_, ok := hash[curr]
 			if !ok {
 				hash[curr] = true
@@ -21,12 +21,8 @@ func helper(nums []int, i int, currMax int, temp []int, result *[][]int, hash ma
 		temp = append(temp, nums[i])
 		helper(nums, i+1, nums[i], temp, result, hash)
 		temp = temp[:len(temp)-1]
-		helper(nums, i+1, currMax, temp, result, hash)
-	} else {
-		helper(nums, i+1, currMax, temp, result, hash)
-		temp = append([]int{}, nums[i])
-		helper(nums, i+1, nums[i], temp, result, hash)
 	}
+	helper(nums, i+1, currMax, temp, result, hash)
 }
 
 func createString(nums []int) string {
