2fd9cf1a5bff7a93b27e486ee9d673541a154f3c
rishavkrm <88821153+rishavkrm@users.noreply.github.com>
2025-09-24 20:45:09 +0530

    Time: 55 ms (24.45%) | Memory: 9.3 MB (24.85%) - LeetSync

--- 322-coin-change/coin-change.go
+++ 322-coin-change/coin-change.go
@@ -1,41 +1,31 @@
 func coinChange(coins []int, amount int) int {
-    dp := make([][]int, amount+1)
-    for i := range(len(dp)){
-        dp[i] = make([]int, len(coins))
-    }
-    for i := range(len(dp)){
-        for j := range(len(dp[0])){
-            dp[i][j] = -2
-        }
-    }
-    res := coinChangeHelper(coins, amount, 0, dp)
-    if res == 10001{
-        return -1
-    }
-    return res
+	dp := make([][]int, len(coins))
+	for i := range len(dp) {
+		dp[i] = make([]int, amount+1)
+	}
+
+	result := helper(coins, amount, 0, &dp)
+	if result == 99999 {
+		return -1
+	}
+	return result
 }
 
-func coinChangeHelper(coins []int, amount int, index int, dp [][]int)int{
-    if amount == 0{
-        return 0
-    }
-    if amount < 0 || index >= len(coins){
-        return 10001
-    }
-    if dp[amount][index] != -2{
-        return dp[amount][index]
-    }
-    // Unbounded knapsack
-    // take and potentially take it again, that is why not increasing index
-    take := 1 + coinChangeHelper(coins, amount - coins[index], index, dp)
-    not_take := coinChangeHelper(coins, amount, index+1, dp)
-    dp[amount][index] =  Min(take, not_take)
-    return dp[amount][index]
-}
-
-func Min(a int, b int) int{
-    if a > b{
-        return b
-    }
-    return a
+func helper(coins []int, amount int, index int, dp *[][]int) int {
+	if amount == 0 {
+		return 0
+	}
+	if amount < 0 || index >= len(coins) {
+		return 99999
+	}
+	if (*dp)[index][amount] != 0 {
+		return (*dp)[index][amount]
+	}
+	mini := 99999
+	for i := index; i < len(coins); i++ {
+		x := 1 + helper(coins, amount-coins[i], i,dp)
+		mini = min(mini, x)
+	}
+	(*dp)[index][amount] = mini
+	return mini
 }
